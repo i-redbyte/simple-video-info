2a118d674d305aa33fea805af18b4a84b616e66f
Ilya Sokolov <developer.sokolov@gmail.com>
2019-12-09 22:21:09 +0300

    add upload file method

--- utils/utils.go
+++ utils/utils.go
@@ -2,8 +2,13 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func Respond(w http.ResponseWriter, data interface{}) {
@@ -19,4 +24,48 @@ func ReturnError(w http.ResponseWriter, err error, code int) {
 	log.Println(err)
 	w.WriteHeader(code)
 	Respond(w, ErrorMessage(err.Error()))
+}
+
+func UploadFile(filePath string, file multipart.File, handler *multipart.FileHeader) error {
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	path := filepath.Join(currentDir, "resources", filePath)
+	fmt.Println("path", path)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		er := os.MkdirAll(path, os.ModePerm)
+		if er != nil {
+			return err
+		}
+	}
+	fileName := handler.Filename
+	log.Printf("Uploaded File: %+v\n", handler.Filename)
+	log.Printf("File Size: %+v\n", handler.Size)
+	log.Printf("MIME Header: %+v\n", handler.Header)
+
+	newFile, err := os.Create(filepath.Join(path, fileName))
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer func() {
+		err := newFile.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	fileBytes, err := ioutil.ReadAll(file)
+	_, err = newFile.Write(fileBytes)
+	if err != nil {
+		return err
+	}
+	return nil
 }
