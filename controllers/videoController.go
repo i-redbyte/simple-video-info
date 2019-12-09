883df4b633f7721d4ca185262fea3c3d0041bc82
Ilya Sokolov <developer.sokolov@gmail.com>
2019-12-09 22:17:45 +0300

    simple server and empty UploadVideo method

---
+++ controllers/videoController.go
@@ -0,0 +1,10 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var UploadVideo = func(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Empty method")
+}
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	c "simple-video-server/controllers"
+)
 
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/uploadVideo", c.UploadVideo).Methods("POST")
+	err := http.ListenAndServe(":4000", handlers.LoggingHandler(os.Stdout, router))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
